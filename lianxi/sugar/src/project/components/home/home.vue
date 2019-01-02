5a566806c0af2a5143e1d614dc4408f82fe6d8b2
SugarSYJ <1697895745@qq.com>
2019-01-02 16:58:43 +0800

    左滑删除，长按编辑删除，点击复制

--- lianxi/sugar/src/project/components/home/home.vue
+++ lianxi/sugar/src/project/components/home/home.vue
@@ -22,9 +22,12 @@
         <div id="group">
             <ul>
                 <li class= "list-item" data-type= "0" v-for= " (item,index) in data" :key="index">
-                    <div class="list-box"  @touchstart="touchStart"  @touchend="touchEnd">
+                    <div class="list-box" @touchstart="touchStart" @touchend="touchEnd" >
                         <div class="personnel">{{item.name}}</div>
-                        <span @click="use($event)">使用</span>
+                        <div class="text">
+                            <span class="text_l">{{item.text}}</span>
+                            <span @click="use($event)">使用</span>
+                        </div>
                     </div>
                     <div class="delete" @click="deletefunction(index,item)" :data-index='index'> 
                         删除
@@ -53,7 +56,9 @@
         name: 'app',
         data () {
             return {
-                data: [{name:'121321313',id:'111'},{name:'121321314',id:'112'},{name:'121321315',id:'113'}]
+                data: [{name:'新春拜年',id:'111',text:'XXX用和飞信给您拜年啦！祝您新年大吉，阖家安康，和乐融融~'},{name:'节日祝福',id:'112',text:'2019年，祝大家新的一年一飞冲天，开工大吉！'},{name:'会议通知',id:'113',text:'XXX会议通知：计划在XXX上午XX点，XX会议室召开会议，会议主题为：XXX，请准时参加'},{name:'推荐和飞信',id:'114',text:'我最近迷上【和飞信】，可以免费发短信、免费打电话、免流量聊天，新人还送300分钟电话时长，福利多多，还不快来一起玩转和飞信？！http://feixin.10086.cn/'}],
+                startX : 0,
+                endX : 0
             }
         },
         methods: {
@@ -68,35 +73,29 @@
             error: function (el) { // 图片加载失败执行方法
                 el.src = '../../images/error.png'
             },
-            //滑动事件
-            touchStart(e) {
-                [this.startX, this.startY] = [e.changedTouches[0].pageX, e.changedTouches[0].pageY];
-                this.rest();
+            //滑动开始
+            touchStart(e){
+            // 记录初始位置
+                this.startX = e.touches[0].clientX;
             },
-            touchEnd(e) {
-                [this.endX, this.endY, this.parentElement] = [e.changedTouches[0].pageX, e.changedTouches[0].pageY, e.target.parentElement];
-                this.parentElement.dataset.type == 0 && (Math.abs(this.endX - this.startX) - Math.abs(this.endY - this.startY)) > 0 ? this.movefunctions(e) : this.parentElement.dataset.type == undefined && (Math.abs(this.endX - this.startX) - Math.abs(this.endY - this.startY)) > 0 ? this.movefunctionss(e) : '';
-            },
-            //若是子级选择器滑动处理
-            movefunctions(e) {
-                if (this.endX - this.startX > 20) {
-                    this.parentElement.dataset.type = 0;
-                    //向右滑动
-                } else if (this.startX - this.endX > 20) {
-                    //向左滑动
-                    this.parentElement.dataset.type = 1;
+            //滑动结束
+            touchEnd(e){
+                // 当前滑动的父级元素
+                let parentElement = e.currentTarget.parentElement;
+                // 记录结束位置
+                this.endX = e.changedTouches[0].clientX;
+                // 左滑
+                if( parentElement.dataset.type == 0 && this.startX - this.endX > 30 ){
+                    this.rest();
+                    parentElement.dataset.type = 1;
                 }
-            },
-            // 若是孙子级选择器滑动处理
-            movefunctionss() {
-                let grandfather = this.parentElement.parentElement;
-                if (this.endX - this.startX > 20) {
-                    //向右滑动
-                    grandfather.dataset.type = 0;
-                } else if (this.startX - this.endX > 20) {
-                    //向左滑动
-                    grandfather.dataset.type = 1;
+                // 右滑
+                if( parentElement.dataset.type == 1 && this.startX - this.endX < -30 ){
+                    this.rest();
+                    parentElement.dataset.type = 0;
                 }
+                this.startX = 0;
+                this.endX = 0;
             },
             //重置
             rest() {
@@ -106,8 +105,9 @@
                 }
             },
             //删除
-            deletefunction(index, item) {
-                console.log(index,item)
+            deletefunction(index,item) {
+                console.log(index,item.id)
+                //this.data.splice(index,1);
                 this.rest();
             },
             //长按弹出删除
@@ -181,87 +181,76 @@
 </script>
 <style lang="scss">
 #group {
-    padding-top: 0.2rem;
     ul {
         width: 100%;
         overflow: hidden;
         list-style-type: none;
         padding-left: 0;
-    }
-    .list-item {
-        position: relative;
-        width: 100%;
-        height: 1.32rem;
-        line-height: 1.32rem;
-        transition: all 0.2s;
-        -ms-transition: all 0.2s;
-        -moz-transition: all 0.2s;
-        -o-transition: all 0.2s;
-        -webkit-transition: all 0.2s ease-in-out;
-        background-color: #fff;
-    }
-    .list-item[data-type="0"] {
-        transform: translate3d(0, 0, 0);
-        -ms-transform:  translate3d(0, 0, 0);
-        -moz-transform:  translate3d(0, 0, 0);
-        -webkit-transform:  translate3d(0, 0, 0);
-        -o-transform:  translate3d(0, 0, 0);
-    }
-    .list-item[data-type="1"] {
-        transform: translate3d(-1.35rem, 0, 0);
-        -ms-transform:  translate3d(-1.35rem, 0, 0);
-        -moz-transform:  translate3d(-1.35rem, 0, 0);
-        -webkit-transform:  translate3d(-1.35rem, 0, 0);
-        -o-transform:  translate3d(-1.35rem, 0, 0);
-    }
-    .list-box {
-        width: 100%;
-        height: 100%;
-        font-size: 0.14rem;
-        display: -webkit-flex;
-        display: flex;
-        justify-content: flex-start;
-        padding-left: 0.48rem;
-  
-    }
-    .list-item .list-box .headImg {
-        height: 0.84rem;
-        margin: auto 0;
-        width: 0.84rem;
-        line-height: 0.84rem;
-        border-radius: 50%;
-        font-size: 0.26rem;
-        color: #fff;
-        text-align: center;
-        /* background-color: rgb(53, 32, 32); */
-    }
-    .list-item .list-box .personnel {
-        height: 0.84rem;
-        width: 2rem;
-        line-height: 0.84rem;
-        font-size: 0.32rem;
-        color: #2a2a2a;
-        padding-left: 0.28rem;
-        /* background: #ff4949; */
-        margin: auto 0;
-    }
-    .list-item .delete {
-        width: 1.35rem;
-        height: 1.32rem;
-        background: #FF3A30;
-        font-size: 17px;
-        color: #fff;
-        text-align: center;
-        line-height: 1.32rem;
-        position: absolute;
-        top: 0;
-        right: -1.35rem;
-    }
-    .concat_line{
-        height:0.02rem;
-        margin-left: 0.48rem;
-        background:rgba(241,241,241,1);
-        margin-top: -0.02rem;
+        margin:0;
+        .list-item {
+            position: relative;
+            width: 100%;
+            transition: all 0.2s;
+            -ms-transition: all 0.2s;
+            -moz-transition: all 0.2s;
+            -o-transition: all 0.2s;
+            -webkit-transition: all 0.2s ease-in-out;
+            background-color: #fff;
+            .list-box {
+                width: 100%;
+                height: 100%;
+                font-size: 0.14rem;
+                padding-left: 0.48rem;
+                font-size: 0.533333rem;
+                .personnel {
+                    height: 0.84rem;
+                    width: 2rem;
+                    line-height: 0.84rem;
+                    font-size: 0.32rem;
+                    color: #2a2a2a;
+                    padding-left: 0.28rem;
+                    margin: auto 0;
+                }
+                .text{
+                    display: -webkit-flex;
+                    display: flex;
+                    justify-content: flex-start;
+                    span{
+                        font-size:40px;
+                    }
+                    .text_l{
+                        width: 80%;
+                        padding-right: 0.133333rem;
+                    }
+                }
+            }
+            .delete {
+                width: 1.35rem;
+                height: 1.32rem;
+                background: #FF3A30;
+                font-size: 17px;
+                color: #fff;
+                text-align: center;
+                line-height: 1.32rem;
+                position: absolute;
+                top: 0;
+                right: -1.35rem;
+            }
+        }
+        .list-item[data-type="0"] {
+            transform: translate3d(0, 0, 0);
+            -ms-transform:  translate3d(0, 0, 0);
+            -moz-transform:  translate3d(0, 0, 0);
+            -webkit-transform:  translate3d(0, 0, 0);
+            -o-transform:  translate3d(0, 0, 0);
+        }
+        .list-item[data-type="1"] {
+            transform: translate3d(-1.35rem, 0, 0);
+            -ms-transform:  translate3d(-1.35rem, 0, 0);
+            -moz-transform:  translate3d(-1.35rem, 0, 0);
+            -webkit-transform:  translate3d(-1.35rem, 0, 0);
+            -o-transform:  translate3d(-1.35rem, 0, 0);
+        }
     }
 }
 </style>
